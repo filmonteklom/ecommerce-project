a3b5ce51a252f318770a56a7c514710de06e673a
filmon teklom <filmonteklom65@gmail.com>
2023-09-25 14:04:11 +0200

    commit App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,31 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header'; 
-import LoginForm from './components/LoginForm/Loginform';
+// import Header from './components/Header/Header'; 
+import LoginForm from './components/LoginForm/LoginForm';
 import RegistrationForm from './components/RegistrationForm/Registration';
 import Layout from './Layout/Layout';
 import Dashboard from './Dashboard/Dashboard/Dashboard';
-import DashboardHeader from './Dashboard/Header/Header';
+// import DashboardHeader from './Dashboard/Header/Header';
+import LogoutButton from './components/LogoutButton/LogoutButton';
 import ForgotPassword from './components/ForgotPassword/ForgotPassword';
 import ResetPassword from './components/ResetPassword/ResetPassword';
+import DashboardLayout from './DashboardLayout/DashboardLayout';
+
 
 function App() {
   return (
     <Router>
       <div>
-        <Header />
+      {/* <Header /> */}
         <Routes>
-        <Route
-            path="/login"
-            element={
-              <Layout>
-                <LoginForm />
-              </Layout>
-            }
-          />
-            <Route
-            path="/register"
-            element={
-              <Layout>
-                <RegistrationForm />
-              </Layout>
-            }
-          />
-           <Route
-            path="/dashboard"
-            element={
-              <Layout header={<DashboardHeader />}>
-                <Dashboard />
-              </Layout>
-            }
-          />
-    <Route path="/forgot-password" component={ForgotPassword} />
-
-    <Route path="/Reset-password" component={ResetPassword} />
+          <Route path="/" element={<Layout />} />
+        <Route path="/login" element={<Layout><LoginForm /></Layout>} />
+        <Route path="/register" element={<Layout><RegistrationForm /></Layout>} />
+ <Route path="/LogoutButton" element={<LogoutButton />} />
+ <Route path="/dashboard" element={<DashboardLayout><Dashboard /></DashboardLayout>} />
+<Route path="/forgotpassword"element={<ForgotPassword />} />    
+<Route path="/Resetpassword/*" element={ <ResetPassword />} />
         </Routes>
       </div>
     </Router>
