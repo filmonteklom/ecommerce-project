44740fc81064a98f8efece07a0701ead510b7011
filmon teklom <filmonteklom65@gmail.com>
2023-09-25 18:47:49 +0200

    commit

--- src/Dashboard/Header/Header.js
+++ src/Dashboard/Header/Header.js
@@ -1,7 +1,7 @@
 // DashboardHeader.js
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, Grid, Hidden } from '@mui/material';
+// import { Link } from 'react-router-dom';
 import LogoutButton from '../../components/LogoutButton/LogoutButton';
 import './Header.css';
 import SearchBar from '../Search/Search';
@@ -14,13 +14,38 @@ function DashboardHeader() {
       
       <AppBar position="static">
       <Toolbar>
-      <SearchBar />
-         <Typography variant="h6" style={{ flexGrow: 1, height: '110px' }}>
-         </Typography>
-         <LogoutButton />
+      <Grid container alignItems="center">
+      <Hidden xsDown>
+              <Grid item sm={6}>
+                <SearchBar />
+              </Grid>
+            </Hidden>
+      {/* <SearchBar /> */}
+
+                  {/* Always visible */}
+                  <Grid item xs={6} sm={4}>
+              <Typography variant="h6" style={{ flexGrow: 1, height: '110px' }}>
+                {/* Your title or logo here */}
+              </Typography>
+            </Grid>
+
+         {/* <Typography variant="h6" style={{ flexGrow: 1, height: '110px' }}>
+         </Typography> */}
+         {/* Hidden on sm and md screens, visible otherwise */}
+         <Hidden smUp>
+              <Grid item xs={6}>
+                <SearchBar />
+              </Grid>
+            </Hidden>
+               {/* Always visible */}
+               <Grid item xs={6} sm={2}>
+              <LogoutButton />
+            </Grid>
+          </Grid>
+         {/* <LogoutButton /> */}
+         {/* </Grid> */}
        </Toolbar>
      </AppBar>
-     
     </div>
     
   );
