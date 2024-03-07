8268786eb7b6718104f3efdca08bc3370d829ea4
filmon teklom <filmonteklom65@gmail.com>
2024-03-07 08:48:19 +0100

    commit chnages packages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import Header from './components/Header/Header'; 
-import LoginForm from './components/LoginForm/LoginForm';
+import LoginForm from './components/LoginForm/loginform';
 import RegistrationForm from './components/RegistrationForm/Registration';
 import Layout from './Layout/Layout';
 import Dashboard from './Dashboard/Dashboard/Dashboard';
